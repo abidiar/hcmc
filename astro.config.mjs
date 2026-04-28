45300105da0711ed7b9c5b1e33e32ae2b1d06ff8
Ali <abidiar@gmail.com>
2026-04-28 14:29:39 -0400

    Fix Netlify asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+const isNetlify = process.env.NETLIFY === 'true';
+
 export default defineConfig({
   integrations: [
     tailwind({
       applyBaseStyles: false
     })
   ],
-  site: 'https://abidiar.github.io',
-  base: '/hcmc'
+  site: isNetlify ? process.env.URL ?? 'https://hoccomuslimcouncil.netlify.app' : 'https://abidiar.github.io',
+  base: isNetlify ? '/' : '/hcmc'
 });
